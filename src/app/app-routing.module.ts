9e3255f723776ce284e72559d2227bdb2c3b69f0
Ber Zelaya <73174174+zelayab@users.noreply.github.com>
2021-05-03 18:48:59 -0300

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ResultadoComponent } from './components/resultado/resultado.component';
+
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'resultado/:valor', component: ResultadoComponent},
+  { path: '**', redirectTo: ' '}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
